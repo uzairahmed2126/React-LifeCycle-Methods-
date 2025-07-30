599ce23ef0df4ab70a471ddef7dd61074512e4d5
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-07-30 18:30:07 +0530

    MountingWithFunctional

--- src/components/MountingWithFunctional/app.jsx
+++ src/components/MountingWithFunctional/app.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+// component mounting
 export default function MountingWithFunctional() {
   const [count, setCount] = useState(0);
 
@@ -11,11 +12,16 @@ export default function MountingWithFunctional() {
   //work on component did update
   useEffect(() => {
     console.log("Mounting on count");
+    return () => {
+      console.log("Unmounting cleanup"); //component will unmount
+    };
   }, [count]);
 
   function handleCount() {
     setCount((prev) => prev + 1);
   }
+
+  // this is render phase and it is work last and it is only used to render the JSX
   return (
     <div>
       <h3>{count}</h3>
